88f165c168782d58dde83fa1825bdbb511372f78
Tom <tom.wautelet@student.unamur.be>
2020-11-23 11:42:36 +0100

    fix

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 import plotly.express as px
-import geopandas as gpd
 import pandas as pd
 
 
