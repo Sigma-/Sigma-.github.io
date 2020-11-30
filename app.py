20520e015a6f7c7560b4f947e46789210e8c4854
Hubercorp <hubertyj@gmail.com>
2020-11-30 01:17:14 +0100

    beaucoup dechose dont ue timelie, du css

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import dash_html_components as html
 from dash.dependencies import Input, Output, State, MATCH, ALL
 import time
 import plotly.graph_objects as go
-
+import plotly.figure_factory as ff
 
 app = dash.Dash(
     external_stylesheets=[dbc.themes.BOOTSTRAP]
@@ -25,11 +25,42 @@ app.config.suppress_callback_exceptions = True
 px.set_mapbox_access_token("pk.eyJ1IjoibmFubzAxIiwiYSI6ImNraHVlYjQ4aDFidzYyeHBiemZlZ2d3d20ifQ.P6e2_ZATNHTAb6BWuadxFw")
 df = pd.read_json(r'Dataset/fossils.json')
 fig = px.scatter_mapbox(df, lat="latitude", lon="longitude", hover_name = "name", hover_data=["old_latitude", "old_longitude"],
-                  color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=1)
-
-df_dino = pd.read_csv(r'Dataset/dinosaurs.csv')
+                  color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=20)
+fig.update_layout(height=650, margin={"r":0,"t":0,"l":0,"b":0}) 
+#trouver une solution pour hauteur en px 
+df_dino = pd.read_csv(r'Dataset/dinosaurs.csv') 
 
 df_dino_names = [dinoname for dinoname in df_dino['dinosaur']]
+
+df_timeline = pd.read_csv(r'Dataset/gts.tsv', sep='\t')
+
+df_timeline_copy = df_timeline.rename(columns={'name':'Task','start':'Start', 'end':'Finish'})
+
+print(df_timeline_copy.iloc[0])
+for i in range(len(df_timeline)):
+    df_timeline["start"][i] += "-01-01"
+    df_timeline["end"][i] += "-12-31"
+    df_timeline["end"][i] = pd.to_datetime(df_timeline["end"][i], errors='coerce')
+    df_timeline["start"][i] = pd.to_datetime(df_timeline["start"][i], errors='coerce')
+
+
+
+#print(df_timeline.head())
+for i in range(len(df_timeline["name"])):
+    if df_timeline["name"][i] == "holocene" and df_timeline["type"][i] == "age":
+        df_timeline["name"][i] = "holocene-age"
+for i in range(len(df_timeline["name"])):
+    if df_timeline["name"][i] == "pridoli" and df_timeline["type"][i] == "age":
+        df_timeline["name"][i] = "pridoli-age"
+colors = dict(zip(df_timeline["name"], df_timeline["color_rgb"]))
+#print(f'len couleur : {len(colors)}')
+#print(f'len couelur dataset : {len(df_timeline["color_rgb"])}')
+
+#print(f'len name : {len(df_timeline["name"])}')
+
+
+fig_timeline = ff.create_gantt(df_timeline_copy, colors = colors, index_col = 'Task', show_colorbar=True, group_tasks=True)
+
 
 
 
@@ -52,22 +83,27 @@ divMap = dcc.Graph(
         figure = fig
     )
 
-divTimeline = html.Div([html.P('Timeline')])
 #Layout à modifier 
 
 app.layout = html.Div(children=[
     html.H1(children='VISUDINO INGEN'),
 
     divSlider,
-
+    
     html.Div(id='genom-list'),
-    dcc.Loading(
-        id="loading-1",
-        type="circle",
-        children = html.Div(id='dino-cards')
-    ),
-    divMap,
-    divTimeline
+    html.Div(id='centre', children=[
+        dcc.Loading(
+            id="loading-1",
+            type="circle",
+            children = html.Div(id='dino-cards')
+        ),
+        divMap,
+        
+    
+    ]),
+    html.Div([
+    dcc.Graph(id="time-line", figure=fig_timeline)
+    ])
     
 ])
 
@@ -160,6 +196,14 @@ def change_map(value1, value2):
         fig.update_layout(transition_duration=500)
         return fig
 
+@app.callback(dash.dependencies.Output("time-line", "figure"), [dash.dependencies.Input("genom-selector", "value")])
+def change_time_line(value):
+    if value == None:
+        fig_timeline = ff.create_gantt(df_timeline_copy.iloc[[0]], colors = colors, index_col = 'Task', show_colorbar=True, group_tasks=True)
+
+        return fig_timeline
+    
+    else:
 
 
 if __name__ == '__main__':
