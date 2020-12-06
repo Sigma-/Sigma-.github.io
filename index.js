111ee9e65e180d24c4da0e97a1b99ca8d1a24749
Jonathan Ferreira <jonathan.ferreira@student.unamur.be>
2020-12-06 08:26:12 +0100

    Changements importants

--- index.js
+++ index.js
@@ -1,31 +1,105 @@
 mapboxgl.accessToken = "pk.eyJ1IjoibmFubzAxIiwiYSI6ImNraHVlYjQ4aDFidzYyeHBiemZlZ2d3d20ifQ.P6e2_ZATNHTAb6BWuadxFw";
+/**
+ * @typedef {{
+ * dinosaur: string,
+ * zone: string,
+ * diet: string,
+ * size: string,
+ * weight: string,
+ * speed: string,
+ * lived: string
+ * }} dinosaur
+ * 
+ * @typedef {{
+ * id: number,
+ * name: string,
+ * rank: string,
+ * early_interval: string,
+ * late_interval: string,
+ * latitude: number,
+ * longitude: number,
+ * old_latitude: number,
+ * old_longitude: number,
+ * country: string,
+ * state: string,
+ * county: string,
+ * formation: string,
+ * lithology: string,
+ * lithology_description: string,
+ * comment: string
+ * }} fossil
+ * 
+ * @typedef {{
+ * name: string,
+ * type: string,
+ * start: number,
+ * end: number,
+ * color_rgb: string,
+ * color_hex: string,
+ * children: time[]
+ * }} time
+ */
+
 
 //Variables globales
+//J'ai mis des doc pour les types pour améliorer l'autocomplete
 /** 
- * @type {any[]} 
- * Attributs de chaque élément dans dino: dinosaur,zone,diet,size,weight,speed,lived
+ * @type {Array<dinosaur>} 
 */
 let dinos;
 let map;
 /**
- * Attributs de chaque élément dans fossils: 
- * id, name (génome + espèce), rank (species/genus), early_interval, late_interval, 
- * latitude, longitude, old_latitude, old_longtitude, 
- * country, state, county,
- * formation, lithology, lithology_description, comment
+ * @type {Array<fossil>}
  */
 let fossils;
 let fossilMarkers = [];
 let popup = new mapboxgl.Popup({
     closeButton: false,
-    closeOnClick: false
+    closeOnClick: true,
+    closeOnMove: true,
+    anchor: "left"
 });
+/**
+ * @type {time}
+ */
 let timeline;
+/**
+ * @type {Object.<string, time>}
+ */
+let flatTimeline = {};
+
 let icicle;
 
-d3.csv("data/dinosaurs.csv").then(function(data) {
-    console.log(data);
+/** @type {Promise<Array<dinosaur>>} */
+let dinoLoader = d3.csv("data/dinosaurs.csv");
+/** @type {Promise<Array<fossil>>} */
+let fossilLoader = d3.json("data/fossils.json");
+/** @type {Promise<Array<time>>} */
+let timelineLoader = d3.json("data/gts_tree.json");
 
+Promise.all([dinoLoader, fossilLoader, timelineLoader]).then(([dinoData, fossilData, timelineData]) => {
+    dinos = dinoData;
+    fossils = fossilData;
+    timeline = timelineData;
+    /* flatTimeline */ buildTimeLineDict(timelineData);
+
+    //Chargement et traitement des données
+    loadDinos(dinoData);
+    loadMap(fossilData);
+    loadTimeline(timelineData);
+
+    //Initialisation
+    /*showDinoList("A");
+    showDinoCard("Allosaurus");*/
+}).catch((error) => {
+    console.log(error);
+    //alert("Une erreur a eu lieu lors du chargement des données.");
+});
+
+/**
+ * @param {dinos} data 
+ */
+function loadDinos(data) {
     let initials = [];
     data.map((dino) => {
         let firstLetter = dino.dinosaur[0];
@@ -35,33 +109,40 @@ d3.csv("data/dinosaurs.csv").then(function(data) {
         }
     });
 
-    dinos = data;
-
     let btnGroup = document.getElementById("letter-slider");
     initials.map((letter) => {
         let btn = document.createElement("button");
-        btn.classList.add("btn", "btn-secondary", "dino-letter");
-        btn.textContent = letter;
+        btn.classList.add("btn", "btn-primary", "dino-letter");
+        btn.textContent = letter.toUpperCase();
         btn.addEventListener("click", (ev) => {
             showDinoList(letter);
         });
         btnGroup.append(btn);
     })
 
-});
+};
 
-d3.json("data/fossils.json").then(function(data) {
-    console.log(data);
+/**
+ * @param {fossils} fossilData
+ */
+function loadMap(fossilData) {
     map = new mapboxgl.Map({
         container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11'
+        style: 'mapbox://styles/mapbox/streets-v11',
+        zoom: 1
     });
 
-    data.forEach((fossil) => {
-        let el = createMarker();
-        el.addEventListener("mouseenter", () => { showFossilData(fossil); });
+    fossilData.forEach((fossil) => {
+        let color = flatTimeline[fossil.early_interval];
+        let el = createMarker(color !== undefined ? {color: color.color_hex} : {});
+        el.addEventListener("mouseenter", () => { 
+            showFossilData(fossil); 
+        });
+        el.addEventListener("mouseleave", () => {
+            popup.remove();
+        })
         el.addEventListener("click", () => {
-            console.log(fossil.early_interval);
+            //console.log(fossil.early_interval);
             zoomToInterval(fossil.early_interval);
         })
         //el.addEventListener("mouseleave", (thisEl) => {});
@@ -73,15 +154,14 @@ d3.json("data/fossils.json").then(function(data) {
         //Lier les données de fossile au marqueur
         marker.fossil = fossil;
         fossilMarkers.push(marker);
-    })
-    fossils = data;
+    });
 
+    map.zoomTo(1);
+    map.setCenter([0,30]);
 
-});
+};
 
-d3.json("data/gts_tree.json").then(function(data) {
-    console.log(data);
-
+function loadTimeline(data) {
     let timelineEl = document.getElementById("timeline");
     let elSize = timelineEl.getBoundingClientRect();
     let width = elSize.width;
@@ -107,14 +187,27 @@ d3.json("data/gts_tree.json").then(function(data) {
         .minSegmentWidth(3)
         (timelineEl);
 
-    timeline = data;
-});
+};
+
+/**
+ * Construit un dictionnaire non-récursif des ères pour un accès facile
+ * @param {time} time 
+ */
+function buildTimeLineDict(time) {
+    let name = time.name.toLowerCase();
+    flatTimeline[name] = time;
+
+    time.children.forEach((child) => {
+        buildTimeLineDict(child);
+    });
+}
 
 function createMarker(options = {}) {
     let defaultOptions = {
-        width: 8,
-        height: 8,
-        color: "#4040D0"
+        width: 10,
+        height: 10,
+        color: "#4040D0",//"rgba(60,60,220,0.8)"//
+        opacity: 0.6
     };
     options = Object.assign(defaultOptions, options);
     let el = document.createElement("div");
@@ -122,7 +215,10 @@ function createMarker(options = {}) {
     el.style.width = `${options.width}px`;
     el.style.height = `${options.height}px`;
     el.style.borderRadius = "100%";
+    el.style.borderWidth = "1px";
+    el.style.borderStyle = "solid";
     el.style.backgroundColor = options.color;
+    el.style.opacity = options.opacity;
 
     return el;
 }
@@ -135,12 +231,12 @@ function showDinoList(letter) {
         dinoSelector.removeChild(dinoSelector.firstChild);
     }
 
-    let validDinos = dinos.filter((dino) => dino.dinosaur[0] === letter);
+    let validDinos = dinos.filter((dino) => dino.dinosaur[0].toLowerCase() === letter.toLowerCase());
 
     let btnGroup = document.getElementById("dino-selector");
     validDinos.map((dino) => {
         let btn = document.createElement("button");
-        btn.classList.add("btn", "btn-secondary", "dino-button");
+        btn.classList.add("btn", "btn-primary", "dino-button");
         btn.textContent = dino.dinosaur;
         btn.addEventListener("click", (ev) => {
             showDinoCard(dino.dinosaur)
@@ -149,14 +245,18 @@ function showDinoList(letter) {
     })
 }
 
+/**
+ * @param {string} dinoname 
+ */
 function showDinoCard(dinoname) {
     let dinoData = dinos.find((dino) => {
-        return dino.dinosaur === dinoname;
+        return dino.dinosaur.toLowerCase() === dinoname.toLowerCase();
     });
 
     /** @type {HTMLImageElement} */
     let img = document.getElementById("dino-image");
     img.src = `img/${dinoData.dinosaur}.png`;
+    document.getElementById("dino-name").textContent = dinoData.dinosaur;
     document.getElementById("dino-feature-zone").textContent = dinoData.zone;
     document.getElementById("dino-feature-diet").textContent = dinoData.diet;
     document.getElementById("dino-feature-size").textContent = dinoData.size;
@@ -175,29 +275,40 @@ function showDinoCard(dinoname) {
     });
 }
 
+/**
+ * @param {fossil} fossil 
+ */
 function showFossilData(fossil) {
     popup
         .setLngLat([fossil.longitude, fossil.latitude])
         .setHTML(`<div>
-        ${fossil.old_latitude}, ${fossil.old_longitude}
+        ${fossil.name} <br />
+        Current pos.: (${fossil.latitude}, ${fossil.longitude}) <br />
+        Old pos.: (${fossil.old_latitude}, ${fossil.old_longitude})
         </div>`)
         .addTo(map);
 }
 
 function zoomToInterval(intervalName) {
-    let intervalData = findIntervalIn(timeline, intervalName);
-    console.log(intervalData);
+    let intervalData = flatTimeline[intervalName];
+    //console.log(intervalData);
     if (intervalData !== undefined) {
         icicle.zoomToNode(intervalData);
     }
 }
 
-function findIntervalIn(data, intervalName){
+/*
+ * Cherche récursivement l'intervalle demandé dans la ligne du temps
+ * @param {time} data 
+ * @param {string} intervalName 
+ * @returns {time}
+ */
+/*function findIntervalIn(data, intervalName){
     if (data.name.toLowerCase() === intervalName.toLowerCase()) {
         return data;
     } else {
         let result = data.children.map((child) => findIntervalIn(child, intervalName));
-        console.log(result);
+        //console.log(result);
         return result.find((value) => value !== undefined);
     }
-}
+}*/
